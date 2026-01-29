84cd08d906d460f8de03715470d8064596801182
Mike-Testut <michael.testut@gmail.com>
2026-01-29 15:28:36 -0800

    hookup langchain and google gemini

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-print("Hi I am your AI assistant, how can I help you today?")
-while True:
-    user_input = input()
-    if user_input == "quit" or user_input == "exit" or user_input == "end":
-        print("Goodbye!")
-        break
-    print(f"Thanks for sharing {user_input}")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_api_key,
+    temperature = 0.5
+)
+
+response = llm.invoke([{"role":"user","content":"Hello how are you"}])
+print(response.content)
+
+# print("Hi I am your AI assistant, how can I help you today?")
+# while True:
+#     user_input = input()
+#     if user_input == "quit" or user_input == "exit" or user_input == "end":
+#         print("Goodbye!")
+#         break
+#     print(f"Thanks for sharing {user_input}")
