6c09c69e5beee732a275dfb8673cd298de5eaf6a
Mike-Testut <michael.testut@gmail.com>
2026-01-30 15:57:09 -0800

    add chat history to ai agent

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+system_prompt="""
+    You are a smart, confident ai assistant.
+    You answer the user's questions in fun and unique ways.
+"""
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -12,13 +19,17 @@ llm = ChatGoogleGenerativeAI(
     temperature = 0.5
 )
 
-response = llm.invoke([{"role":"user","content":"Hello how are you"}])
-print(response.content)
+history=[]
+prompt = ChatPromptTemplate.from_messages([("system", system_prompt),
+                             MessagesPlaceholder("history"),
+                             ("user", "{input}")])
 
-# print("Hi I am your AI assistant, how can I help you today?")
-# while True:
-#     user_input = input()
-#     if user_input == "quit" or user_input == "exit" or user_input == "end":
-#         print("Goodbye!")
-#         break
-#     print(f"Thanks for sharing {user_input}")
+chain = prompt | llm
+
+print("Hi I am your AI assistant, how can I help you today?")
+while True:
+    user_input = input()
+    response = chain.invoke({"input":user_input, "history":history})
+    history.append(HumanMessage(user_input))
+    history.append(AIMessage(response.content))
+    print(response.content)
